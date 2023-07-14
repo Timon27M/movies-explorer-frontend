77765358a33d4354abb60b17a0aac9ddded23900
Timofey Pichugin <tpich87@gmail.com>
2023-07-14 20:40:21 +0300

    feat: старт проекта

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import logo from '../../images/logo.svg';
+import './App.css';
+import Header from "../Header/Header.jsx"
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import "./Header.css"
+import { Link } from "react-router-dom";
+import logoImg from "../../images/logo.svg";
+import NavigationAuth from "../NavigationAuth/NavigationAuth";
+
+function Header() {
+  return (
+  <header className="header">
+        <Link className="header__link">
+            <img className="header__logo" src={logoImg} alt="Логотип сайта"></img>
+        </Link>
+        <NavigationAuth />
+    </header>
+  );
+}
+
+export default Header;
