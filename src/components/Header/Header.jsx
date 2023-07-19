acffcbf4f77f6aa6e9ab773d44a5339f619179bb
Timofey Pichugin <tpich87@gmail.com>
2023-07-19 20:53:24 +0300

    feat: добавлено боковое меню

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,18 @@
 import "./Header.css"
-import { Link } from "react-router-dom";
+import { Link, useLocation } from 'react-router-dom';
 import logoImg from "../../images/logo.svg";
 import NavigationAuth from "../NavigationAuth/NavigationAuth";
+import NavigationSite from "../NavigationSite/NavigationSite";
 
 function Header() {
+  const { pathname } = useLocation();
+
   return (
-  <header className="header header_type_auth">
+  <header className={`header ${pathname === '/' ? 'header_type_auth' : 'header_type_site'}`}>
         <Link className="header__link">
             <img className="header__logo" src={logoImg} alt="Логотип сайта"></img>
         </Link>
-        <NavigationAuth />
+        { pathname === '/' ? <NavigationAuth /> :<NavigationSite />}
     </header>
   );
 }
---
+++ src/components/NavigationSite/NavigationSite.jsx
@@ -0,0 +1,60 @@
+import "./NavigationSite.css";
+import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import imgAcc from "../../images/img-Acc.svg";
+import imgButtonMenu from "../../images/icon-buttonMenu.svg";
+import imgCloseButtonMenu from "../../images/closeButtonMenu.svg";
+
+function NavigationSite() {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const showMenu = () => {
+    setOpenMenu(!openMenu);
+  };
+
+  return (
+    <div className="navigationSite">
+      <button className="navigationSite__button" onClick={showMenu}>
+        <img
+          className="navigationSite__img-button"
+          src={imgButtonMenu}
+          alt="иконка"
+        />
+      </button>
+      <div
+        className={`navigationSite__container ${
+          openMenu ? "navigationSite__container_active" : ""
+        }`}
+      >
+        <div className="navigationSite__content">
+          <div className="navigationSite__main">
+            <NavLink className="navigationSite__main-item navigationSite__main-item_type_home">Главная</NavLink>
+            <NavLink className="navigationSite__main-item navigationSite__main-item_type_all-films">Фильмы</NavLink>
+            <NavLink className="navigationSite__main-item navigationSite__main-item_type_my-films">
+              Сохранённые фильмы
+            </NavLink>
+          </div>
+          <div className="navigationSite__acc">
+            <NavLink className="navigationSite__profile">
+              <p className="navigationSite__profile-text">Аккаунт</p>
+              <img
+                className="navigationSite__profile-img"
+                src={imgAcc}
+                alt="фавикон"
+              />
+            </NavLink>
+          </div>
+          <button className="navigationSite__close-button" onClick={showMenu}>
+            <img
+              className="navigationSite__close-img-button"
+              src={imgCloseButtonMenu}
+              alt="иконка"
+            />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default NavigationSite;
